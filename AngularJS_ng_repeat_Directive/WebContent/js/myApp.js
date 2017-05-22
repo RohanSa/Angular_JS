301af78f865b84d3044ccdf9ff011884777d3cf7
rohan <salunkherohan.j@gmail.com>
2017-05-23 00:59:41 +0530

    Angular JS ng-repeat directive examples

---
+++ AngularJS_ng_repeat_Directive/WebContent/js/myApp.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller("RepeatController", function($scope) {
+	
+	var students = [
+			{id : 1, name :"Amit"},
+			{id : 2, name :"Vivek"},
+			{id : 3, name :"Yogesh"}
+	];
+	
+	$scope.students = students;
+})
