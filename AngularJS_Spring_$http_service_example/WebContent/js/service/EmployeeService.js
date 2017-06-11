32e9dc33c5f84ee4f61eb3f1ed6889ba3cae3827
rohan <salunkherohan.j@gmail.com>
2017-06-12 00:33:23 +0530

    Removed unwanted code

--- AngularJS_Spring_$http_service_example/WebContent/js/service/EmployeeService.js
+++ AngularJS_Spring_$http_service_example/WebContent/js/service/EmployeeService.js
@@ -15,43 +15,11 @@ angular.module('myApp').factory(
 							addEmployee : addEmployee
 					};
 					
-					
-					
-					
-					
 					function getEmployees() {
 						
-						return $http.get('employee/getallemployees.spring')
-								.then(function(response) {
-									$log.info('Success: EmployeeService.getAllEmployees()'+response);
-									$log.info('Success: EmployeeService.getAllEmployees()'+response.data);
-									$log.info(response.data);
-									return response ;
-
-								}, function(response) {
-									$log.info('Error: EmployeeService.getAllEmployees()'+response);
-									$log.info('Error: EmployeeService.getAllEmployees()'+response.data);
-									$log.info(response);
-									return response;
-								});
+						return $http.get('employee/getallemployees.spring');
 					};
 					
-				/*	function addEmployee(formdata) {
-						
-						return $http.post('employee/addemployee.spring',formdata)
-								.then(function(response) {
-									$log.info('Success: EmployeeService.addEmployees()'+response);
-									$log.info('Success: EmployeeService.addEmployees()'+response.data);
-									$log.info(response.data);
-									return response ;
-
-								}, function(response) {
-									$log.info('Error: EmployeeService.addEmployees()'+response);
-									$log.info('Error: EmployeeService.addEmployees()'+response.data);
-									$log.info(response);
-									return response;
-								});
-					};*/
 					
 					function addEmployee(formdata) {
 						
