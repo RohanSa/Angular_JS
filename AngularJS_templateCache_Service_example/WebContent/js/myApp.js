120c35ceb83e0cb89157199a048b50b29db46294
rohan <salunkherohan.j@gmail.com>
2017-06-23 00:42:39 +0530

    Angular JS $templateCache service example

---
+++ AngularJS_templateCache_Service_example/WebContent/js/myApp.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller('TemplatecahceController', function($scope, $timeout, $log) {
+
+	$scope.message = 'Template Cache';
+
+});
+
+myApp.run(function($templateCache) {
+	
+	$templateCache.put('startup.html', '<strong>Template is loaded !!</strong>');
+})
