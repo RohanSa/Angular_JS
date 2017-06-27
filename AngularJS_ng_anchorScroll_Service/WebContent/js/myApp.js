95d02364b4d8cc43e577f9d25a4e4831e0f9e726
rohan <salunkherohan.j@gmail.com>
2017-06-27 23:36:58 +0530

    Added $anchorScrollProvider part

--- AngularJS_ng_anchorScroll_Service/WebContent/js/myApp.js
+++ AngularJS_ng_anchorScroll_Service/WebContent/js/myApp.js
@@ -2,12 +2,20 @@
  * 
  */
 
-var myApp = angular.module("myApp", []).controller("ScrollController",
+var myApp = angular.module("myApp", []);
+
+myApp.config([ '$anchorScrollProvider', function($anchorScrollProvider) {
+	$anchorScrollProvider.disableAutoScrolling(true);
+}]);
+
+myApp.controller("ScrollController",
 		function($scope, $location, $anchorScroll) {
 
 			$scope.goTo = function(location) {
 
 				$location.hash(location);
+				/*If automatic scrolling is disabled, one must explicitly call $anchorScroll() 
+				 * in order to scroll to the element related to the current hash.*/
 
 				$anchorScroll();
 			};
